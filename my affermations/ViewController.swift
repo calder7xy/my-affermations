066f5dcb3b45cbed9a5a12b23bc2c14a4b96d212
calder7xy <52419388+calder7xy@users.noreply.github.com>
2019-07-02 11:34:29 -0500

    Initial Commit

---
+++ my affermations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my affermations
+//
+//  Created by SER-YCL4 on 7/2/19.
+//  Copyright © 2019 SER-YCL4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
